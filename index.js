ec84944fff3e3a297edf3efcdf042b7d5856b96a
uday-g-ithub15 <mudayhasan@gmail.com>
2022-05-28 03:36:58 +0600

    Get single part

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 app.use(cors())
 app.use(express.json())
+require('dotenv').config()
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jb74a.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run = async () => {
+    try {
+        await client.connect()
+        const partsCollection = client.db(`dronemanufacture`).collection('parts');
+
+        //Load all parts
+        app.get('/parts', async (req, res) => {
+            const query = {};
+            const cursor = partsCollection.find(query)
+            const parts = await cursor.toArray()
+            res.send(parts)
+        })
+        //Load single part
+        app.get('/parts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const part = await partsCollection.findOne(query)
+            res.send(part);
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
 
 app.get('/', async (req, res) => {
     res.send('Welcome')
